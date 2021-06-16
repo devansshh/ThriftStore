0955a72fd22da03eeae6ee8e777748c0c5189c8a
devansshh <18ucs078@lnmiit.ac.in>
2021-06-16 16:38:35 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+require('dotenv'). config();
+const express=require('express');
+
+const app=express();
+
+PORT= process.env.PORT|| 5000;
+
+app.listen(PORT, ()=> console.log(`server running at ${PORT}`));
